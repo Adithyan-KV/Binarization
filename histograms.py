6582f881b9b6cbe5b1bf90d1ccdcf9703e6c6f1e
adithyan-kv <adithyankv007@gmail.com>
2020-10-10 01:56:53 +0530

    Added graph for threshold vs variance
     -A plot showing the variation of interclass variance with threshold
      is being made
     -Factored it into a function

--- histograms.py
+++ histograms.py
@@ -13,6 +13,9 @@ def greyscale_histogram(image):
     luma_image_data = np.matmul(image_data, weights_array)
     hist, bins = np.histogram(luma_image_data, bins=256)
     plt.bar(bins[:-1], hist, color='grey', width=1)
+    plt.title("Image histogram")
+    plt.xlabel("Luma Value")
+    plt.ylabel("Number of pixels with luma value")
     plt.show()
 
 
@@ -27,6 +30,9 @@ def color_histogram(image):
     plt.bar(g_bins[:-1], g_hist, color='green', width=1, alpha=0.5)
     b_hist, b_bins = np.histogram(blue_channel, bins=256)
     plt.bar(b_bins[:-1], b_hist, color='blue', width=1, alpha=0.5)
+    plt.xlabel("Color Values")
+    plt.ylabel("Number of pixels with color values")
+    plt.legend(['red', 'green', 'blue'])
     plt.show()
 
 
